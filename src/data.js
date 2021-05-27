8a7b3817c0b50fb6ef4aea4bea56c7a65939a593
David Jeffers <68712230+DavidJeffers@users.noreply.github.com>
2021-05-27 15:35:31 -0500

    res
    yes

---
+++ src/data.js
@@ -0,0 +1,124 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+        { name: "Solar Cove",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/d7ee494a68e5ff81082460680db4944f2ade6497-300x300.jpg",
+      artist: "Mama Aiuto",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=16179",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+      { name: "Cruising",
+      cover:
+        "https://f4.bcbits.com/img/a1362578403_10.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=17087",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    { name: "Low Rider",
+      cover:
+        "https://f4.bcbits.com/img/a1362578403_10.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=17090",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+         name: "Vintage",
+      cover:
+        "https://f4.bcbits.com/img/a1362578403_10.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=17088",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    { name: "Dive",
+      cover:
+        "https://f4.bcbits.com/img/a1362578403_10.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=17089",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    { name: "Simplexity",
+      cover:
+        "https://f4.bcbits.com/img/a1362578403_10.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=14958",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    //ADD MORE HERE
+  ];
+}
+
+export default chillHop;
+
